fe510478fc18451597e67c72741373b06eff82d5
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2019-10-21 16:41:00 +0300

    Updated the client-side files.

--- GeneticAlgNetControl/wwwroot/js/site.js
+++ GeneticAlgNetControl/wwwroot/js/site.js
@@ -2,3 +2,82 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(window).on('load', () => {
+
+    // Check if there is a dropdown menu form on the page.
+    if ($('.dropdown-menu-form').length !== 0) {
+        // Add a listener for licking inside the dropdown menu.
+        $('.dropdown-menu.dropdown-menu-form').on('click', (event) => {
+            // Stop the propagation.
+            event.stopPropagation();
+        });
+    }
+
+    // Check if there is a list group of items on the page.
+    if ($('.item-group').length !== 0) {
+        // Define a function which gets all of the selected items and creates a JSON string array with their IDs.
+        const updateSelectedItems = (groupElement) => {
+            // Get all of the list group items.
+            const items = $(groupElement).find('.item-group-item');
+            // Remove the active class from all list items.
+            $(items).removeClass('active');
+            // Go over all of the checked elements and get the corresponding list group items.
+            const selectedItems = $(groupElement).find('input[type="checkbox"]:checked').closest('.item-group-item');
+            // Go over each of the selected items and mark them as active.
+            $(selectedItems).addClass('active');
+            // Check how many elements are selected.
+            if (selectedItems.length === 0) {
+                // Disable the group buttons.
+                $('.item-group-button').prop('disabled', true);
+                // Unmark the checkbox as indeterminate.
+                $(groupElement).find('.item-group-select').prop('indeterminate', false);
+                // Uncheck the checkbox.
+                $(groupElement).find('.item-group-select').prop('checked', false);
+            } else {
+                // Enable the group buttons.
+                $('.item-group-button').prop('disabled', false);
+                // Check if not all elements are selected.
+                if (selectedItems.length !== items.length) {
+                    // Mark the checkbox as indeterminate.
+                    $(groupElement).find('.item-group-select').prop('indeterminate', true);
+                } else {
+                    // Unmark the checkbox as indeterminate.
+                    $(groupElement).find('.item-group-select').prop('indeterminate', false);
+                    // Check the checkbox.
+                    $(groupElement).find('.item-group-select').prop('checked', true);
+                }
+            }
+        };
+        // Add a listener for when a checkbox gets checked or unchecked.
+        $('.item-group').on('change', 'input[type="checkbox"]', (event) => {
+            // Get the current list group.
+            const groupElement = $(event.target).closest('.item-group');
+            // Update the selected items.
+            updateSelectedItems(groupElement);
+        });
+        // Add a listener for the select checkbox.
+        $('.item-group-select').on('change', (event) => {
+            // Get the current list group.
+            const groupElement = $(event.target).closest('.item-group');
+            // Check if the checkbox is currently checked.
+            if ($(event.target).prop('checked')) {
+                // Check all of the checkboxes on the page.
+                $(groupElement).find('input[type="checkbox"]:not(:checked)').prop('checked', true);
+            } else {
+                // Uncheck all of the checkboxes on the page.
+                $(groupElement).find('input[type="checkbox"]:checked').prop('checked', false);
+            }
+            // Update the selected items.
+            updateSelectedItems(groupElement);
+        });
+        // On page load, parse the input and check the group items.
+        (() => {
+            // Go over all of the groups.
+            $('.item-group').each((index, element) => {
+                // Update the selected items.
+                updateSelectedItems(element);
+            });
+        })();
+    }
+
+});
