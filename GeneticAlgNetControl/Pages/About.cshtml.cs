50eeee136f39d950a3a23765446cf759aec605b9
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2019-10-21 12:04:16 +0300

    Updated the main pages and fixed small UI bugs.

---
+++ GeneticAlgNetControl/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeneticAlgNetControl.Pages
+{
+    [AllowAnonymous]
+    public class AboutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            // Return the page.
+            return Page();
+        }
+    }
+}
