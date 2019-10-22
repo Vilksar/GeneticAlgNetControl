29e319fe0085694945252644c3945f68c7ea4e68
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2019-10-22 11:30:06 +0300

    Updated all tuples to classes.

---
+++ GeneticAlgNetControl/Helpers/Models/DateTimePeriod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneticAlgNetControl.Helpers.Models
+{
+    public class DateTimePeriod
+    {
+        public DateTime DateTimeStarted { get; set; }
+
+        public DateTime? DateTimeEnded { get; set; }
+    }
+}
---
+++ GeneticAlgNetControl/Helpers/Models/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneticAlgNetControl.Helpers.Models
+{
+    public class Edge
+    {
+        public string SourceNode { get; set; }
+
+        public string TargetNode { get; set; }
+    }
+}
