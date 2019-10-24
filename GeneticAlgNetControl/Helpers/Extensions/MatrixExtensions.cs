37e77b078b5483d9051f33425f3fa9ea5bd5eacb
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2019-10-24 11:29:36 +0300

    Added the model for a chromosome.

---
+++ GeneticAlgNetControl/Helpers/Extensions/MatrixExtensions.cs
@@ -0,0 +1,71 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneticAlgNetControl.Helpers.Extensions
+{
+    /// <summary>
+    /// Provides extensions for matrices.
+    /// </summary>
+    public static class MatrixExtensions
+    {
+        /// <summary>
+        /// Checks if the given matrix is full rank or not.
+        /// </summary>
+        /// <param name="matrix">The matrix to check.</param>
+        /// <returns>True if the matrix is full rank, false otherwise</returns>
+        public static bool IsFullRank(this Matrix<double> matrix)
+        {
+            // Clone the given matrix.
+            var clonedMatrix = matrix.Clone();
+            // Check if there are more rows than columns.
+            if (clonedMatrix.ColumnCount < clonedMatrix.RowCount)
+            {
+                // Create a new helper matrix.
+                var helperMatrix = Matrix<double>.Build.Dense(clonedMatrix.RowCount, clonedMatrix.RowCount - clonedMatrix.ColumnCount);
+                // Append the helper matrix to the cloned matrix.
+                clonedMatrix = clonedMatrix.Append(helperMatrix);
+            }
+            // Transpose the matrix.
+            clonedMatrix = clonedMatrix.Transpose();
+            // If needed, Insert a number of all-zero columns such that to make the matrix square (only if it has less columns than rows).
+            for (int index = 0; index < matrix.RowCount - matrix.ColumnCount; index++)
+            {
+                matrix = matrix.InsertColumn(matrix.ColumnCount, Vector<double>.Build.Dense(matrix.RowCount, 0.0));
+            }
+            // Check if it is full rank.
+            return clonedMatrix.QR(QRMethod.Full).IsFullRank;
+        }
+
+        /// <summary>
+        /// Computes the rank of the given matrix.
+        /// </summary>
+        /// <param name="matrix">The matrix to check.</param>
+        /// <returns>The rank of the matrix.</returns>
+        public static int GetFullRank(this Matrix<double> matrix)
+        {
+            // Clone the given matrix.
+            var clonedMatrix = matrix.Clone();
+            // Check if there are more rows than columns.
+            if (clonedMatrix.ColumnCount < clonedMatrix.RowCount)
+            {
+                // Create a new helper matrix.
+                var helperMatrix = Matrix<double>.Build.Dense(clonedMatrix.RowCount, clonedMatrix.RowCount - clonedMatrix.ColumnCount);
+                // Append the helper matrix to the cloned matrix.
+                clonedMatrix = clonedMatrix.Append(helperMatrix);
+            }
+            // Transpose the matrix.
+            clonedMatrix = clonedMatrix.Transpose();
+            // If needed, Insert a number of all-zero columns such that to make the matrix square (only if it has less columns than rows).
+            for (int index = 0; index < matrix.RowCount - matrix.ColumnCount; index++)
+            {
+                matrix = matrix.InsertColumn(matrix.ColumnCount, Vector<double>.Build.Dense(matrix.RowCount, 0.0));
+            }
+            // Check if it is full rank.
+            return clonedMatrix.QR(QRMethod.Full).R.Diagonal().Count(item => item != 0);
+        }
+    }
+}
---
+++ GeneticAlgNetControl/Helpers/Models/Chromosome.cs
@@ -0,0 +1,244 @@
+﻿using GeneticAlgNetControl.Data.Enumerations;
+using GeneticAlgNetControl.Helpers.Extensions;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneticAlgNetControl.Helpers.Models
+{
+    /// <summary>
+    /// Represents a chromosome used by the algorithm.
+    /// </summary>
+    public class Chromosome
+    {
+        /// <summary>
+        /// Represents the fitness of the chromosome.
+        /// </summary>
+        private double? _fitness = null;
+
+        /// <summary>
+        /// Represents the unique control nodes in the chromosome.
+        /// </summary>
+        private IEnumerable<string> _uniqueControlNodes = null;
+
+        /// <summary>
+        /// Represents the genes of the chromosome, as a dictionary of a target node to its control node.
+        /// </summary>
+        public Dictionary<string, string> Genes { get; set; }
+
+        /// <summary>
+        /// Constructor for the chromosome starting from the target nodes.
+        /// </summary>
+        /// <param name="targetNodes">The target nodes for the algorithm.</param>
+        public Chromosome(List<string> targetNodes)
+        {
+            // Assign to each gene its default value (the corresponding target node).
+            Genes = targetNodes.ToDictionary(item => item, item => item);
+        }
+
+        /// <summary>
+        /// Constructor for the chromosome starting from a different chromosome.
+        /// </summary>
+        /// <param name="chromosome">The chromosome from which to build a new one.</param>
+        public Chromosome(Chromosome chromosome)
+        {
+            // Assign to each gene its default value (the corresponding target node).
+            Genes = chromosome.Genes.ToDictionary(item => item.Key, item => item.Key);
+        }
+
+        /// <summary>
+        /// Computes and gets the unique control nodes in the chromosome.
+        /// </summary>
+        /// <returns>The unique control nodes in the chromosome.</returns>
+        public IEnumerable<string> GetUniqueControlNodes()
+        {
+            // Compute the unique control nodes if they weren't already computed.
+            _uniqueControlNodes = _uniqueControlNodes ?? Genes.Values.Distinct();
+            // Return them.
+            return _uniqueControlNodes;
+        }
+
+        /// <summary>
+        /// Computes and gets the fitness of the chromosome.
+        /// </summary>
+        /// <returns>The fitness of the chromosome.</returns>
+        public double GetFitness()
+        {
+            // Compute the fitness if it wasn't already computed.
+            _fitness = _fitness ?? GetUniqueControlNodes().Count();
+            // Return it.
+            return _fitness.Value;
+        }
+
+        /// <summary>
+        /// Checks if the chromosome is a solution or not.
+        /// </summary>
+        /// <param name="nodeIndices">The dictionary containing, for each node, its index in the node list.</param>
+        /// <param name="matrixPowerList">The list containing the different powers of the matrix (CA, CA^2, CA^3, ... ).</param>
+        /// <returns>True if the chromosome is a solution, false otherwise</returns>
+        public bool IsValid(Dictionary<string, int> nodeIndices, List<Matrix<double>> matrixPowerList)
+        {
+            // Get the unique control nodes.
+            var uniqueControlNodes = Genes.Values.Distinct().ToList();
+            // Initialize the B matrix.
+            var matrixB = Matrix<double>.Build.Dense(nodeIndices.Count(), uniqueControlNodes.Count());
+            // Go over each control node.
+            for (int index = 0; index < uniqueControlNodes.Count(); index++)
+            {
+                // Update the corresponding field.
+                matrixB[nodeIndices[uniqueControlNodes[index]], index] = 1.0;
+            }
+            // Initialize the R matrix.
+            var matrixR = Matrix<Double>.Build.DenseOfMatrix(matrixPowerList[0]).Multiply(matrixB);
+            // Repeat until we reach the maximum power.
+            for (int index = 1; index < matrixPowerList.Count(); index++)
+            {
+                // Compute the current power matrix.
+                matrixR = matrixR.Append(matrixPowerList[index].Multiply(matrixB));
+            }
+            // Return the validity.
+            return matrixR.IsFullRank();
+        }
+
+        /// <summary>
+        /// Initializes the chromosome with randomly generated gene values between the lower and the upper limit.
+        /// </summary>
+        /// <param name="nodeIndices">The dictionary containing, for each node, its index in the node list.</param>
+        /// <param name="pathList">The list containing, for each target nodes, the nodes from which it can be reached.</param>
+        /// <param name="matrixPowerList">The list containing the different powers of the matrix (CA, CA^2, CA^3, ... ).</param>
+        /// <param name="lowerLimit">The lower limit of the interval in which to randomly generate the values.</param>
+        /// <param name="upperLimit">The upper limit of the interval in which to randomly generate the values.</param>
+        /// <param name="random">The random seed.</param>
+        public void Initialize(Dictionary<string, int> nodeIndices, Dictionary<string, List<string>> pathList, List<Matrix<double>> matrixPowerList, int lowerLimit, int upperLimit, Random random)
+        {
+            // Define the number of tries in which to try and find a valid chromosome.
+            var tries = 10;
+            // Get the genes for which to generate randomly the values.
+            var genesRandom = Genes.Keys.ToList().GetRange(lowerLimit, upperLimit - lowerLimit).ToList();
+            // Repeat while the chromosome is not valid.
+            while (genesRandom.Any())
+            {
+                // Decrease the number of tries.
+                tries--;
+                // Go over all of the values in the random interval.
+                foreach (var item in genesRandom)
+                {
+                    Genes[item] = pathList[item][random.Next(pathList[item].Count())];
+                }
+                // Check if the chromosome is valid.
+                if (IsValid(nodeIndices, matrixPowerList))
+                {
+                    // Exit the loop.
+                    break;
+                }
+                // Check if we reached the last try.
+                else if (tries == 0)
+                {
+                    // Reset the number of tries.
+                    tries = 10;
+                    // Get a random gene to remove from the list of genes to generate randomly.
+                    var randomGene = genesRandom[random.Next(genesRandom.Count())];
+                    // Assign to it the default value.
+                    Genes[randomGene] = randomGene;
+                    // Remove it from the list of genes to generate randomly.
+                    genesRandom.Remove(randomGene);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a new offspring chromosome from this and a second chromosome parents.
+        /// </summary>
+        /// <param name="secondChromosome">The second parent chromosome of the offspring.</param>
+        /// <param name="nodeIndices">The dictionary containing, for each node, its index in the node list.</param>
+        /// <param name="matrixPowerList">The list containing the different powers of the matrix (CA, CA^2, CA^3, ... ).</param>
+        /// <param name="type">The crossover type for the algorithm.</param>
+        /// <returns></returns>
+        public Chromosome Crossover(Chromosome secondChromosome, Dictionary<string, int> nodeIndices, List<Matrix<double>> matrixPowerList, Dictionary<string, bool> nodePreferred, AlgorithmCrossoverType type, Random random)
+        {
+            // Define a new chromosome.
+            var chromosome = new Chromosome(this);
+            // Define the number of tries in which to try and find a valid chromosome.
+            var tries = 10;
+            // Get the number of occurances of each gene in this chromosome and which genes of each are preferred.
+            var occurancesInFirst = GetUniqueControlNodes().ToDictionary(item => item, item => Genes.Count(item1 => item1.Value == item));
+            var occurancesInSecond = secondChromosome.GetUniqueControlNodes().ToDictionary(item => item, item => Genes.Count(item1 => item1.Value == item));
+            // Use the specified crossover type.
+            switch (type)
+            {
+                // If we have a standard crossover.
+                case AlgorithmCrossoverType.Standard:
+                    // Repeat while the chromosome is not valid.
+                    while (tries > 0)
+                    {
+                        // Decrease the number of tries.
+                        tries--;
+                        // Assign to each gene in the chromosome its corresponding random parent value.
+                        chromosome.Genes = chromosome.Genes.ToDictionary(item => item.Key, item => random.NextDouble() < occurancesInFirst[item.Value] / occurancesInFirst[item.Value] + occurancesInSecond[item.Value] ? Genes[item.Key] : secondChromosome.Genes[item.Key]
+                        );
+                        // Check if the chromosome is valid.
+                        if (chromosome.IsValid(nodeIndices, matrixPowerList))
+                        {
+                            // Exit the loop.
+                            break;
+                        }
+                    }
+                    // End the switch statement.
+                    break;
+                // If we have a crossover with preference.
+                case AlgorithmCrossoverType.WithPreference:
+                    // Repeat while the chromosome is not valid.
+                    while (tries > 0)
+                    {
+                        // Decrease the number of tries.
+                        tries--;
+                        // Assign to each gene in the chromosome its corresponding random parent value.
+                        chromosome.Genes = chromosome.Genes.ToDictionary(item => item.Key, item =>
+                        {
+                            // Check if the gene in any of the chromosomes is a preferred node.
+                            var isPreferredFirst = nodePreferred[Genes[item.Key]];
+                            var isPreferredSecond = nodePreferred[secondChromosome.Genes[item.Key]];
+                            // Check if the first corresponding gene is preferred, and the second one isn't.
+                            if (isPreferredFirst && !isPreferredSecond)
+                            {
+                                // Return it.
+                                return Genes[item.Key];
+                            }
+                            // Check if the second corresponding gene is preferred, and the first one isn't.
+                            else if (!isPreferredFirst && isPreferredSecond)
+                            {
+                                // Return it.
+                                return secondChromosome.Genes[item.Key];
+                            }
+                            // If they both have the same state, use the same return instruction as if there were no preferred nodes.
+                            return random.NextDouble() < occurancesInFirst[item.Value] / occurancesInFirst[item.Value] + occurancesInSecond[item.Value] ? Genes[item.Key] : secondChromosome.Genes[item.Key];
+                        });
+                        // Check if the chromosome is valid.
+                        if (chromosome.IsValid(nodeIndices, matrixPowerList))
+                        {
+                            // Exit the loop.
+                            break;
+                        }
+                    }
+                    // End the switch statement.
+                    break;
+                // If we have none of the above.
+                default:
+                    // Set the tries to 0, such that the new chromosome will inherit completely one of the parents.
+                    tries = 0;
+                    // End the switch statement.
+                    break;
+            }
+            // Check if the new chromosome is still not valid.
+            if (tries == 0)
+            {
+                // Choose randomly a parent to give all of its genes.
+                chromosome.Genes = random.NextDouble() < 0.5 ? Genes : secondChromosome.Genes;
+            }
+            // Return the new chromosome.
+            return chromosome;
+        }
+    }
+}
